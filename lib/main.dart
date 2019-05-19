e924b51a05e3ca74f75f272964dcc0540d34840d
TonySu <sutongok2015@gmail.com>
2019-05-19 17:08:09 -0500

    update

---
+++ lib/main.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'story_brain.dart';
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+StoryBrain stories = StoryBrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/background.png'), fit: BoxFit.cover),
+        ),
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+          constraints: BoxConstraints.expand(),
+          child: SafeArea(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Expanded(
+                  flex: 12,
+                  child: Center(
+                    child: Text(
+                      stories.getStory(),
+                      style: TextStyle(
+                        fontSize: 25.0,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: FlatButton(
+                    onPressed: () {
+                      setState(() {
+                        stories.nextStory(1);
+                      });
+                    },
+                    color: Colors.red,
+                    child: Text(
+                      stories.getChoice1(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Visibility(
+                    visible: stories.buttonSHouldVisible(),
+                    child: FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          stories.nextStory(2);
+                        });
+                      },
+                      color: Colors.blue,
+                      child: Text(
+                        stories.getChoice2(),
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
